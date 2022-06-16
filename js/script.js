02cc3ecaa76a99793d9f9554caa58be159e0efe8
lramdhan <luzirmdhn@gmail.com>
2022-06-16 13:25:03 +0700

    ambil data mhs

--- js/script.js
+++ js/script.js
@@ -4,8 +4,16 @@ searching.addEventListener('change', event => {
 });
 
 let data;
-
 fetch('www.kemendikbud.com?univ=all')
+    .then(res => {
+        data = JSON.parse(res);
+    })
+    .catch(res => {
+        alert(res);
+    });
+
+let mhs;
+fetch('www.kemendikbud.com?mhs=all')
     .then(res => {
         data = JSON.parse(res);
     })
